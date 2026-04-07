e815937816514d9b614319c1822fb50a5d47d2c7
beki255 <bereket2553@gmail.com>
2026-04-08 01:33:59 +0300

    Update API URL for production

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // Create an axios instance with base URL
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'https://mini-crm-backend-y1ho.onrender.com';
 
 const api = axios.create({
   baseURL: API_URL,
